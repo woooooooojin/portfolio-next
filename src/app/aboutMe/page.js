5c9994478ca39b8f1aede23f1ddc3fca16c9a020
woooooooojin <kevin2205@naver.com>
2023-12-22 11:01:58 +0900

    first commit

---
+++ src/app/aboutMe/page.js
@@ -0,0 +1,10 @@
+import Home from "../page";
+
+export default function aboutMe() {
+  return (
+    <>
+      <Home/>
+      <h1>aboutme</h1>
+    </>
+  )
+}
---
+++ src/app/project/page.js
@@ -0,0 +1,6 @@
+
+export default function Project() {
+  return (
+    <div>프로젝트</div>
+  )
+}
